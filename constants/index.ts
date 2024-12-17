f7dba07af4a83e1e8d16eddd79c558d0e03b9168
rmitKay <s4018445@student.rmit.edu.au>
2024-12-17 17:21:35 +1100

    style(icons): Adds more icons to icon list

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,13 @@
 import back_arrow from "@/assets/icons/back_arrow.png";
 import mail from "@/assets/icons/mail_fill.png";
 import lock from "@/assets/icons/lock.png";
+import person from "@/assets/icons/person.png";
+import google from "@/assets/icons/google.png";
 
 export const icons = {
   back_arrow,
   mail,
   lock,
+  person,
+  google,
 };
