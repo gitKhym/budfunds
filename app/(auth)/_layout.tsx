c74d1e3e2ca201836dfd1567f3549a6c171c8390
rmitKay <s4018445@student.rmit.edu.au>
2024-12-17 17:24:16 +1100

    style(color): add consistent bg color to stacks

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,9 +3,14 @@ import { Stack } from 'expo-router';
 const Layout = () => {
 
   return (
-    <Stack>
+    <Stack screenOptions={{
+      contentStyle: {
+        backgroundColor: "#232227"
+      }
+    }}>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
 }
