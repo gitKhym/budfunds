775d2885bf0897e7cc277a190e93a24018101753
rmitKay <s4018445@student.rmit.edu.au>
2024-12-25 01:14:49 +1100

    style(buttons): allows icon components as icons in buttons

--- components/onboarding/CustomButton.tsx
+++ components/onboarding/CustomButton.tsx
@@ -1,15 +1,15 @@
-import { Image, Text, TouchableOpacity } from "react-native"
+import { Text, TouchableOpacity } from "react-native"
 
 import { CustomButtonProps } from "@/types/type";
 
-const CustomButton = ({ label, icon, onPress, additionalStyle }: CustomButtonProps) => {
+const CustomButton = ({ label, Icon, onPress, additionalStyle }: CustomButtonProps) => {
   return (
-    <TouchableOpacity onPress={onPress} className="flex-row items-center gap-4  rounded-square bg-grey-800 p-4">
-      {icon &&
-        <Image source={icon} className="h-12 w-12" />
+    <TouchableOpacity onPress={onPress} className={`${additionalStyle} flex-row items-center justify-center gap-4  rounded-square bg-grey-800 p-4`}>
+      {Icon &&
+        <Icon />
       }
       {label &&
-        <Text className="font-Lexend text-lg text-white">{label}</Text>
+        <Text className="font-Lexend text-sm-half text-white">{label}</Text>
       }
     </TouchableOpacity>
   )
--- components/onboarding/HeroButton.tsx
+++ components/onboarding/HeroButton.tsx
@@ -1,16 +1,16 @@
 import { Text, TouchableOpacity } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 
-import { ButtonProps } from "@/types/type";
+import { CustomButtonProps } from "@/types/type";
 
 // TODO: Add icon
 
-const HeroButton = ({ label, icon, onPress, additionalStyle }: ButtonProps) => {
+const HeroButton = ({ label, Icon, onPress, additionalStyle }: CustomButtonProps) => {
   return (
     <TouchableOpacity onPress={onPress}>
       <LinearGradient colors={["#FF405D", "#E45593"]} style={{ borderRadius: 10 }} end={[1, 0.5]} className={`${additionalStyle} p-4`}>
-
-        <Text className="text-center font-Lexend text-lg text-white">{label}</Text>
+        {Icon && <Icon />}
+        <Text className="text-center font-Lexend text-sm-half text-white">{label}</Text>
       </LinearGradient>
     </TouchableOpacity>
   );
