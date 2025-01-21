d306efbbba9144b53905cb7f87860647656ba1d7
rmitKay <s4018445@student.rmit.edu.au>
2025-01-21 19:11:48 +1100

    adds typing

--- types/type.d.ts
+++ types/type.d.ts
@@ -4,6 +4,28 @@ import {
   TouchableOpacityProps,
 } from "react-native";
 
+declare module "react-native-switch-selector" {
+  import { Component } from "react";
+  import { ViewStyle, TextStyle } from "react-native";
+
+  interface SwitchSelectorProps {
+    options: { label: string; value: string | number }[];
+    initial: number;
+    onPress: (value: string | number) => void;
+    textColor?: string;
+    selectedColor?: string;
+    borderColor?: string;
+    height?: number;
+    hasPadding?: boolean;
+    style?: ViewStyle;
+    textStyle?: TextStyle;
+    selectedTextStyle?: TextStyle;
+    buttonTextStyle?: TextStyle;
+  }
+
+  export default class SwitchSelector extends Component<SwitchSelectorProps> {}
+}
+
 declare interface IconProps {
   size: number;
   fill?: string;
@@ -23,13 +45,14 @@ declare interface TextFieldProps {
 
 declare interface CustomButtonProps {
   label?: string;
-  Icon?: any;
+  to?: string;
+  params?: object;
+  Icon?: React.ElementType;
   image?: ImageSourcePropType;
-  additionalStyle?: string;
+  additionalStyle?: Falsy | ViewStyle;
+  additionalLabelStyle?: Falsy | TextStyle;
   onPress: () => void | void;
 }
-
-declare interface Friend extends User {}
 
 declare interface User {
   username: string;
@@ -38,4 +61,37 @@ declare interface User {
   date?: string;
   isOwed: boolean;
   avatar: string;
+}
+
+declare interface Friend extends User {}
+
+declare interface SplitGroup {
+  createdAt: string;
+  id: string;
+  role: string;
+  splitGroup: {
+    id: string;
+    splitGroupName: string;
+    createdAt: string;
+    updatedAt: string;
+    members: Array<{
+      [key: string]: any;
+    }>;
+  };
+  splitGroupId: string;
+  updatedAt: string;
+  userId: string;
+}
+
+declare interface Split {
+  createdAt: Date;
+  updatedAt: Date;
+  id: string;
+  splitName: string;
+  isSettled: boolean;
+  participants: Object[];
+  author: {
+    amountOwed: number;
+    isSettled: boolean;
+  };
 }
